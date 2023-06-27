8a4fb50fc1b5ddb9bd2132be69da87e67a0f2486
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-06-27 14:06:03 +0100

    Add 10-loops.js

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+export default function appendToEachArrayValue(array, appendString) {
+  let idx = 0;
+
+  for (const item of array) {
+    array[idx] = appendString + item;
+    idx++;
+  }
+
+  return array;
+}
