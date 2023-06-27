7104f215a23f47cb7286433a1a9fa27ec4e1a2c6
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-06-27 13:12:28 +0100

    Update 7-getBudgetObject.js

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -2,7 +2,11 @@
  * Object property--value shorthand
  */
 export default function getBudgetObject(income, gdp, capita) {
-  const budget = { income, gdp, capita };
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
 
   return budget;
 }
