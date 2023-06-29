c58e3e53b52b8c54484faa71107457b7c939bb5a
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-06-29 13:38:47 +0100

    Fixed semicolon, finally() and parentheses issues in 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,4 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({ status: 200, body: 'success' });
-    } else {
-      reject(new Error());
-    }
-  }).then(() => console.log('Got a response from the API')).catch();
+  promise.then(() => ({ status: 200, body: 'success' })).catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
