0577c13802353a7fadc839f5dd8d3f705a45e9bc
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-06-29 21:44:32 +0100

    Add 3-all.js

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+/**
+ * Collectively resolve all promises
+ */
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()]).then((result) => {
+    console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
+  }).catch(() => {
+    console.log('Signup system offline');
+  });
+}
