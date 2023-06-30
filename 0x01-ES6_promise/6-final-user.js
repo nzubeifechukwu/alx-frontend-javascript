4b02f321dacd45c0a73149b8ea0ebaa7fe0482be
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-06-30 02:43:20 +0100

    Add 6-final-user.js

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,9 @@
+/**
+ * Promise.allSettled()
+ */
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignUp(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+}
