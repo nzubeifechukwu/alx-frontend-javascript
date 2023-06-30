44d76afecd03d08bf89059846d6362481272e335
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-06-30 03:06:20 +0100

    Change Error object to String in return value

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,5 +5,9 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignUp(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  const promise = Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  return promise.then((result) => result.map((r) => ({
+    status: r.status,
+    value: r.status === 'fulfilled' ? r.value : String(r.reason),
+  })));
 }
