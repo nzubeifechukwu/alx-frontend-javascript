34bcea064a0222249ca42fb72b77636e15af8a84
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-06-30 03:34:50 +0100

    Add 8-try.js

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,9 @@
+/**
+ * Throw error
+ */
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0');
+  }
+  return (numerator / denominator);
+}
