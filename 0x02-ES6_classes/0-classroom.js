ea9ee84686f2275b7b3be591e88f690009aa39ea
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-07-01 04:08:12 +0100

    Add 0-classroom.js

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,9 @@
+/**
+ * Implement a class named ClassRoom. It should accept one attribute named
+ * `maxStudentsSize` (number), assigned to _maxStudentsSize
+ */
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
