9a2a6b482bf269e695043e23d0bf033980d87f9d
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-07-01 16:50:13 +0100

    Fix issue with _currency intialisation

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -6,7 +6,7 @@ import Currency from './3-currency';
 export default class Pricing {
   constructor(amount, currency) {
     this._amount = amount;
-    this._currency = new Currency(currency._code, currency._name);
+    this._currency = currency;
   }
 
   get amount() {
