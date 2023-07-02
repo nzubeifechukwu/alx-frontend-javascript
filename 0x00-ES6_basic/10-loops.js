984a4a80b5fe5b3f5f63bf5aa72eac2b4478a0c6
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-07-02 20:42:38 +0100

    Update 10-loops.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -2,8 +2,8 @@ export default function appendToEachArrayValue(array, appendString) {
   const arrayCopy = array.slice();
 
   for (const item of arrayCopy) {
-    array.splice(arrayCopy.indexOf(item), 1, appendString + item);
+    arrayCopy.splice(array.indexOf(item), 1, appendString + item);
   }
 
-  return array;
+  return arrayCopy;
 }
