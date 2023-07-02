b0438ce1160f39bcdccf60a72d179d4f543132d0
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-07-02 20:04:24 +0100

    Update 4-rest-parameter.js

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -2,9 +2,5 @@
  * Return the number of arguments passed using the rest parameter syntax
  */
 export default function returnHowManyArguments(...args) {
-  let total = 0;
-  for (const arg of args) {
-    total += 1;
-  }
-  return total;
+  return args.length;
 }
