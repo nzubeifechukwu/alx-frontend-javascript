e1f449e1cf89dae2317331c2a5a837c20b23d8b8
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-07-02 05:20:33 +0100

    Add 6-sky_high.js

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,23 @@
+/**
+ * SkyHighBuilding class extends Building
+ */
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() { // eslint-disable-line
+    return 'Evacuate slowly the 60 floors';
+  }
+}
