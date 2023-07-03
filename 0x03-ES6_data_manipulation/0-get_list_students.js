d2452a8d6e48faebb732a5b71ad3ea1a4d7c7704
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-07-03 16:08:04 +0100

    Add 0-get_list_students.js

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,16 @@
+/**
+ * function getListStudents()
+ *
+ * Return: an array of objects each with properties id, firstName, location
+ */
+export default function getListStudents() {
+  return [{
+    id: 1, firstName: 'Guillaume', location: 'San Francisco',
+  },
+  {
+    id: 2, firstName: 'James', location: 'Columbia',
+  },
+  {
+    id: 5, firstName: 'Serena', location: 'San Francisco',
+  }];
+}
