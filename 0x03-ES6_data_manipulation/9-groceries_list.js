69dd40d0e81d5713c94b8d2933d83969277c4c98
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-07-04 19:18:59 +0100

    Add 9-groceries_list.js

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,7 @@
+/**
+ * function groceriesList()
+ * Return a map of groceries each with a given name and quantity
+ */
+export default function groceriesList() {
+  return new Map([['Apples', 10], ['Tomatoes', 10], ['Pasta', 1], ['Rice', 1], ['Banana', 5]]);
+}
