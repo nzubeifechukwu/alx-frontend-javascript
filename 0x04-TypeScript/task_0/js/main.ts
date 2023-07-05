b0746f7e8d9218c72ebdeb9bd22a46ebf36c2f74
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-07-05 20:53:16 +0100

    Update main.ts

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -8,18 +8,40 @@ interface Student {
 
 // Student objects
 const student_1: Student = {
-  firstName: 'Nzube',
-  lastName: 'Ifechukwu',
+  firstName: "Nzube",
+  lastName: "Ifechukwu",
   age: 31,
-  location: 'Nigeria',
+  location: "Nigeria",
 };
 
 const student_2: Student = {
-  firstName: 'Sylvia',
-  lastName: 'Jitti',
-  age: '25',
-  location: 'Nigeria',
+  firstName: "Sylvia",
+  lastName: "Jitti",
+  age: 25,
+  location: "Nigeria",
 };
 
 // studentsList is an array of students
 const studentsList = [student_1, student_2];
+
+const table = document.createElement("table");
+const tableBody = document.createElement("tbody");
+
+for (let i = 0; i < studentsList.length; i++) {
+  const row = document.createElement("tr");
+
+  for (let j = 0; j < 2; j++) {
+    const cell = document.createElement("td");
+    const cellText = document.createTextNode(student_1.firstName);
+    cell.appendChild(cellText);
+    row.appendChild(cell);
+  }
+
+  tableBody.appendChild(row);
+}
+
+table.appendChild(tableBody);
+
+document.body.appendChild(table);
+
+table.setAttribute("body", "2");
